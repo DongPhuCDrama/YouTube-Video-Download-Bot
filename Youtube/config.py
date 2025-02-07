d0a6859e96fdca6654d3e1dbfc881fa0ec7e5620
DongPhuCDrama <86602147+DongPhuCDrama@users.noreply.github.com>
2025-02-07 12:18:20 +0700

    config.py

--- Youtube/config.py
+++ Youtube/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7143519009:AAFerJrf0MKAe1kuXLuHmDiGLyvBhGUxvC4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6997371758:AAENkbJIIq8eH2aFUuq67ttJWx3hqHcYe7Y")
     API_ID = int(os.environ.get("API_ID", "23407106"))
     API_HASH = os.environ.get("API_HASH", "34817fe096f567f55ce37750b8a5ff1b")
     #Add your channel id. For force Subscribe.
